38649ffab9d1341c145344f03c77b8afdb661561
duleendra <shashimald@gmail.com>
2023-06-28 07:35:55 +0800

    Add Order app

--- app/order/server.js
+++ app/order/server.js
@@ -1,27 +1,26 @@
 import express from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
 // Sample data - Replace with your own data source or database connection
-const customers = [
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Smith' },
-    { id: 3, name: 'Bob Johnson' }
+const orders = [
+    { id: 1, customer_id: 1, price: 2500 },
+    { id: 2, customer_id: 2, price: 300 }
 ];
 
-// Endpoint to retrieve all customers
-app.get('/customers', (req, res) => {
-    res.json(customers);
+// Endpoint to retrieve all users
+app.get('/orders', (req, res) => {
+    res.json(orders);
 });
 
 // Endpoint to retrieve a specific user by ID
-app.get('/customers/:id', (req, res) => {
+app.get('/orders/customer/:id', (req, res) => {
     const { id } = req.params;
-    const customer = customers.find(u => u.id === parseInt(id));
+    const user = orders.find(o => o.customer_id === parseInt(id));
 
-    if (customer) {
-        res.json(customer);
+    if (user) {
+        res.json(user);
     } else {
         res.status(404).json({ message: 'User not found' });
     }
